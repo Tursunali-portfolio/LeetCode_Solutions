85132be16db2bed26aeb00f2b8272e4ec7669486
Tursunali-portfolio <79641787+Tursunali-portfolio@users.noreply.github.com>
2021-04-04 01:01:12 +0500

    Update 1748. Sum of Unique Elements.py

--- 1748. Sum of Unique Elements.py
+++ 1748. Sum of Unique Elements.py
@@ -1,17 +1,5 @@
 class Solution:
     def sumOfUnique(self, nums: List[int]) -> int:
-        usum = 0
-        tmp = []
-        cnum = nums.copy()
-        for num in cnum:
-            if num in tmp:
-                continue
-            if num in nums:
-                tmp.append(num)
-                nums.remove(num)
-            if num in nums:
-                while num in nums:
-                    nums.remove(num)
-            else:
-                usum += num
-        return usum
+        uniq = []
+        [uniq.append(num) for num in nums if nums.count(num) == 1]
+        return sum(uniq)
